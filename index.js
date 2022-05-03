7ca2c6096094908f41ba9a606b2247c223af360f
alyb237 <alyb237@gmail.com>
2022-05-03 19:44:48 +0200

    Fix more errors

--- index.js
+++ index.js
@@ -27,26 +27,3 @@ if (process.argv.length < 3) {
     )(myBox),
   );
 }
-// console.log(
-//   chalk.hex(randomColor({
-//       luminosity: process.argv[3],
-//       hue: process.argv[2],
-//     })))(myBox),
-
-//console.log(chalk.green('#' + Math.floor(Math.random() * 16777215).toString()));
-
-//import randomColor from 'randomcolor';
-
-/*generates random color
-
-randomColor({
-  luminosity: 'random',
-  hue: 'red',
-});
-
-//let color = randomColor();
-//console.log(color);
-console.log(randomColor.hue);
-/*--------------------------
-//console.log('#' + Math.floor(Math.random() * 16777215).toString(16));
-*/
